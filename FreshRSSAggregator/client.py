4c277201dcccef6dd50a84bc72256a814f5939c9
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2025-04-29 16:24:43 +0900

    追加・修正

--- FreshRSSAggregator/client.py
+++ FreshRSSAggregator/client.py
@@ -1,4 +1,8 @@
+import os
 from freshrss_api import FreshRSSAPI
+import logging
+import datetime
+import google.generativeai as genai
 
 class FreshRSSAggregator():
     api_client = None
@@ -14,4 +18,35 @@ class FreshRSSAggregator():
         
         self.api_client = FreshRSSAPI(host, username, password, verify_ssl, verbose)
     
+    def Fetch(self, hoursDelta: int = 24, gemini_api_key:str = None, gemini_model_name:str = None):
+        if gemini_api_key == None:
+            gemini_api_key = os.environ.get("GEMINI_API_KEY")
+        
+        if gemini_model_name == None:
+            gemini_model_name = os.environ.get("GEMINI_MODEL_NAME")
 
+        unread_items = self.api_client.get_unreads()
+        filtered_items = self.FilterItems(hoursDelta, unread_items)
+
+        prompts = ['以下のリストに示すニュースを要約してください。']
+        for item in filtered_items:
+            prompts += "- " + item.title
+
+        prompt = '\n'.join(prompts)
+
+        genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+        gemini_pro = genai.GenerativeModel("gemini-2.0-flash")
+
+        response = gemini_pro.generate_content(prompt)
+
+        return(response)
+
+    def FilterItems(self, hoursDelta:int, unread_items):
+        now = datetime.datetime.now()
+        threshold_time = now - datetime.timedelta(hours=hoursDelta)
+
+        filtered_items = [
+            item for item in unread_items
+            if datetime.datetime.fromtimestamp(item.created_on_time) > threshold_time]
+            
+        return filtered_items
--- testclient.py
+++ testclient.py
@@ -1,7 +1,26 @@
+import os
+from os.path import join, dirname 
 from FreshRSSAggregator import client
+from dotenv import load_dotenv
+import logging
+import google.generativeai as genai
+import datetime
+
+logger = None
 
 def main():
-    client.FreshRSSAggregator()
+    load_dotenv(verbose=True)
+
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    rssc = client.FreshRSSAggregator(os.environ.get("HOST"), os.environ.get("USERNAME"), os.environ.get("PASSWORD"))
+
+    response = rssc.Fetch(gemini_api_key=os.environ.get("GEMINI_API_KEY"), gemini_model_name=os.environ.get("GEMINI_MODEL_NAME"))
+    print(response.text)
 
 if __name__ == "__main__":
+    
+    logging.basicConfig()
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
+
     main()
